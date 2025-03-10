05c875956e34ec2350611738695aadef2e037af4
Jay Raval <141604465+Jay-Raval-19@users.noreply.github.com>
2025-03-10 22:34:31 +0530

    Routes Updated in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+import UserDashboard from "./pages/UserDashboard";
+import CreateProposal from "./pages/CreateProposal";
+import ProposalDetailPage from "./pages/ProposalDetailPage";
+import DelegateSelection from "./pages/DelegateSelection";
+import ProposalHistory from "./pages/ProposalHistory";
 import "./index.css";
-import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/userdash" element={<UserDashboard />} />
+        <Route path="/create-proposal" element={<CreateProposal />} />
+        <Route path="/proposal/:id" element={<ProposalDetailPage />} />
+        <Route path="/delegates" element={<DelegateSelection />} />
+        <Route path="/history" element={<ProposalHistory />} />
+      </Routes>
+    </Router>
   );
 }
 
